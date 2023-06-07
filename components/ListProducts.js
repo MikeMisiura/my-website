b3506448a992eb78ce0ee15e22cad5d15872cf89
Mike Misiura <mikemisiura@gmail.com>
2023-06-07 14:10:00 -0600

    project data and card

---
+++ components/ListProducts.js
@@ -0,0 +1,19 @@
+import { projects } from '@/data/products.json'
+import Card from 'react-bootstrap/Card';
+
+export default function ListProducts() {
+    return projects.map((p) => {
+        if (!p.display) return
+        return (
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={p.screenshot} />
+            <Card.Body>
+                <Card.Title>{p.title}</Card.Title>
+                <Card.Text>{p.description}</Card.Text>
+                <Card.Link href={p.link}>View {p.title}</Card.Link>
+                <Card.Link href={p.github}>Github for {p.title}</Card.Link>
+            </Card.Body>
+        </Card>
+        )
+    })
+}
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,9 +1,12 @@
 import styles from '@/styles/projects.module.css'
+import ListProducts from '@/components/ListProducts'
 
 export default function Projects() {
+
     return (
         <div className={styles.nav}>
             <h1>Projects</h1>
+            <ListProducts />
         </div>
     )
 }
